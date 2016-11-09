99c5c6dea94ba6ccb20af8037409bb098b2d3658
Alex Miroshnyk <miroshnik.alex@gmail.com>
2016-11-09 11:41:40 +0000

    new rules have been added for http, ssh and icmp

--- demo-5/securitygroup.tf
+++ demo-5/securitygroup.tf
@@ -3,6 +3,11 @@ data "aws_ip_ranges" "european_ec2" {
   services = [ "ec2" ]
 }
 
+data "aws_ip_ranges" "us_ec2" {
+  regions = [ "us-west-1", "us-east-1", "us-east-2", "us-west-2" ]
+  services = [ "ec2" ]
+}
+
 resource "aws_security_group" "from_europe" {
  name = "from_europe"
 
@@ -12,6 +17,26 @@ resource "aws_security_group" "from_europe" {
     protocol = "tcp"
     cidr_blocks = [ "${data.aws_ip_ranges.european_ec2.cidr_blocks}" ]
   }
+  ingress {
+    from_port = "any"
+    to_port = "22"
+    protocol = "tcp"
+    cidr_blocks = ["${data.aws_ip_ranges.european_ec2.cidr_blocks}"]
+  }
+  ingress {
+    from_port = "any"
+    to_port = "80"
+    protocol = "tcp"
+    cidr_blocks = ["${data.aws_ip_ranges.european_ec2.cidr_blocks}"]
+  }
+  ingress {
+    from_port = "any"
+    to_port = "any"
+    protocol = "icmp"
+    cidr_blocks = ["${data.aws_ip_ranges.european_ec2.cidr_blocks}"]
+    cidr_blocks = ["${data.aws_ip_ranges.us_ec2.cidr_blocks}"]
+  }
+
   tags {
     CreateDate = "${data.aws_ip_ranges.european_ec2.create_date}"
     SyncToken = "${data.aws_ip_ranges.european_ec2.sync_token}"
