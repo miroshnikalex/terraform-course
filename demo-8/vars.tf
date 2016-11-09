5cf191226dede8ec43c5ad8a988a34fc2480eae1
Alex Miroshnyk <miroshnik.alex@gmail.com>
2016-11-09 14:23:18 +0000

    fixed path to ssh keys

--- demo-8/vars.tf
+++ demo-8/vars.tf
@@ -2,10 +2,10 @@ variable "AWS_REGION" {
   default = "eu-west-1"
 }
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
+  default = ".ssh-aws/mykey"
 }
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+  default = ".ssh-aws/mykey.pub"
 }
 variable "AMIS" {
   type = "map"
